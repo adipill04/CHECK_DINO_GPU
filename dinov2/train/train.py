bf539b1e54df985ace695d29d520045095e29769
Aditya Pillai <107209683+adipill04@users.noreply.github.com>
2024-04-18 08:10:36 -0500

    add comment

--- dinov2/train/train.py
+++ dinov2/train/train.py
@@ -292,8 +292,12 @@ def do_train(cfg, model, resume=False):
             logger.info("NaN detected")
             raise AssertionError
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        #wandb logging
         wandb.log({"loss": losses_reduced})
         wandb.log({"learning rate": lr})
+
+        
         metric_logger.update(lr=lr)
         metric_logger.update(wd=wd)
         metric_logger.update(mom=mom)
